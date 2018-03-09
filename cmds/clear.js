9d0124ee95269f269e33821b17985199b0176f7d
Odar <radoradev770@gmail.com>
2018-03-09 22:29:50 +0200

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("You don't have premssions to do that!");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premssions to do that!");
+  if(!args[0]) return message.channel.send("Please enter a number of messages to clear! `Usage: !clear <amount>`");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`**__Cleared ${args[0]} messages.__**`).then(msg => msg.delete(2000));
 });
