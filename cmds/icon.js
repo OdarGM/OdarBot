1f6bd268bce7295c200015e0b78175d0a53df360
Odar <radoradev770@gmail.com>
2018-03-09 22:31:00 +0200

    Update icon.js

--- cmds/icon.js
+++ cmds/icon.js
@@ -1,14 +1,18 @@
+const Discord = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
 let msg = await message.channel.send("Generating icon...");
 
 if(!message.guild.iconURL) return msg.edit("No icon found!");
 
-await message.channel.send({files: [
-    {
-        attachment: message.guild.iconURL,
-        name: "icon.png"
-    }
-]});
+let iconembed = new Discord.RichEmbed()
+.setColor("00ff00")
+.setFooter("Searched by " + message.author.tag)
+.setImage(message.guild.iconURL)
+.setTitle("Icon")
+.setDescription("[Icon URL link]("+message.guild.iconURL+")")
+
+message.channel.send(iconembed)
     
     msg.delete();
  }
