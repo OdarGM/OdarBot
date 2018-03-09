9f09c5b5cd010a62e8fd11f9bd723957edd852ea
Odar <radoradev770@gmail.com>
2018-03-09 22:27:34 +0200

    Update botinfo.js

--- cmds/botinfo.js
+++ cmds/botinfo.js
@@ -1,16 +1,23 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    let inline = true
     let bicon = bot.user.displayAvatarURL;
+    let usersize = bot.users.size
+    let chansize = bot.channels.size
+    let uptimxd = bot.uptime 
+    let servsize = bot.guilds.size
     let botembed = new Discord.RichEmbed()
-    .setDescription("**__Bot Information__**")
     .setColor("#00ff00")
     .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Bot Created by:", "<@291221132256870400>" )
+    .addField("Bot Name", bot.user.username, inline)
+    .addField("Bot Owner", "<@291221132256870400>", inline )
+    .addField("Servers", servsize, inline)
+    .addField("Channels", chansize, inline)
+    .addField("Users", usersize, inline)
+    .addField("Uptime", uptimxd, inline)
     .addField("Created On", bot.user.createdAt)
     
-
     message.channel.send(botembed);
 
     message.delete();
