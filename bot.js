73a7c45e3744cd597f7d9133b3dce139ec7bfbe4
Odar <radoradev770@gmail.com>
2018-03-10 16:09:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ console.log(`Bots is ready and working in ${bot.guilds.size} servers with ${bot.
 
 bot.user.setStatus('Online')
 
-bot.user.setActivity("Odar Army | !help");
+bot.user.setActivity(`!help | ${bot.guilds.size} `);
 
 try {
     let link = await bot.generateInvite(["ADMINISTRATOR"]);
