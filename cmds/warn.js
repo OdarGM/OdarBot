3e72d2866fad76908aa02870eee08bb91d58417c
Odar <radoradev770@gmail.com>
2018-03-11 22:38:05 +0200

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -1,47 +1,26 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const fs = require("fs");
-const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-module.exports.run = async (bot, message, args) => {
+exports.run = (client, message, args) => {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premission to do that!");
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.');
+  if (reason.length < 1) return message.reply('You must have a reason for the warning.');
 
-//example: !warn @user <reason>
-if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You need the `manage members` premission to do that!");
-let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-if(!wUser) return message.reply("Plase tag an user to warn!");
-if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't warn this user!");
-let reason = args.join(" ").slice(22);
-
-if(!warns[wUser.id]) warns[wUser.id] = {
-    warns: 0
-};
-
-    warns[wUser.id].warns++;
-
-    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-        if (err) console.log(err)
-      });
-    
-let warnEmbed = new Discord.RichEmbed()
-  .setDescription("Warns")
-  .setAuthor(message.author.username)
+  let dmsEmbed = new Discord.RichEmbed()
+  .setTitle("Warn")
   .setColor("#00ff00")
-  .addField("Warned User", `<@${wUser.id}>`)
-  .addField("Warned In", message.channel)
-  .addField("Number of Warnings", warns[wUser.id].warns)
+  .setDescription(`You have been warned on \`${message.guild.name}\``)
+  .addField("Warned by", message.author.tag)
   .addField("Reason", reason);
 
-  let warnchannel = message.guild.channels.find(`name`, "logs");
-  if(!warnchannel) return message.reply("Couldn't find channel");
-
-  warnchannel.send(warnEmbed);
-
-  wUser.send(`You have been warned in ${message.guild.name} with reason ${message.send.reason}`)
+  user.send(dmsEmbed);
 
   message.delete();
 
 }
 
-module.exports.help = {
-  name: "warn"
-}
+exports.help = {
+  name: 'warn'
+};
