65852f23e2555fb06eee310af868ae6f629e471f
Odar <radoradev770@gmail.com>
2018-03-15 22:39:15 +0200

    Create buy.js

---
+++ cmds/buy.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    let inline = true
+    let bicon = bot.user.displayAvatarURL;
+ 
+    let embed = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .setTitle("Buy Discord Bot!")
+    .setThumbnail(bicon)
+    .addField("You can order your own discord bot from", "<@291221132256870400>")
+    .addField("Bot Commands", "Same as this bot", inline)
+    .addField("XP System", "Optional", inline)
+    .addField("Private bot", "Yes only the bot owner can invite it!")
+    .addField("Pricing", "€20 Per Bot", inline)
+    .addField("Order", "To order the bot use `!order`", inline)
+    .addField("Questions", "If you have any questions feel free to ask in the [Support Server](https://discord.gg/zvvasbc)")
+
+    message.channel.send(embed)
+
+}
+    module.exports.help = {
+        name: "buy"
+}
