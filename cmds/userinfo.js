b03b75d167c5308822f18d6bae952df8c0c87b28
Odar <radoradev770@gmail.com>
2018-03-18 23:07:46 +0200

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,21 +1,39 @@
 const Discord = require("discord.js");
 
 module.exports.run =async (bot, message, args) => {
+    let inline = true
+    let resence = true
+    const status = {
+        online: "<:online:424890369688469504> Online",
+        idle: "<:idle:424890472855502849> Idle",
+        dnd: "<:dnd:424890429524410368> Do Not Disturb",
+        offline: "<:offilne:424890400319340546> Offline/Invisible"
+      }
         
 const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 let target = message.mentions.users.first() || message.author
 
+if (member.user.bot === true) {
+    bot = "<:bot:424913647173894145> Yes";
+  } else {
+    bot = "<:user:424958082691629057> No";
+  }
+
             let embed = new Discord.RichEmbed()
                 .setAuthor(member.user.username)
-                .setDescription("This the user's info!")
                 .setThumbnail((target.displayAvatarURL))
                 .setColor("#00ff00")
                 .addField("Full Username", 
-    `${member.user.username}#${member.user.discriminator}`)
-                .addField("ID", member.user.id)
+    `${member.user.tag}`, inline)
+                .addField("ID", member.user.id, inline)
+                .addField("Nickname", `${member.nickname !== null ? `<:approve:424890484025196565> Nickname: ${member.nickname}` : "<:deny:424890501758451712> None"}`, true)
+                .addField("Bot", `${bot}`,inline, true)
+                .addField("Status", `${status[member.user.presence.status]}`, inline, true)
+                .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "<:deny:424890501758451712> Not playing"}`,inline, true)
+                .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:deny:424890501758451712> No Roles"}`, true)
                 .addField("Joined Discord At", member.user.createdAt)
     
-            message.channel.send({embed: embed});
+            message.channel.send(embed);
 
             message.delete();
     }
