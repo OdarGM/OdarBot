b9995a7cf1a5761cae90df65ebb970f142038133
Odar <radoradev770@gmail.com>
2018-03-18 23:11:51 +0200

    Create flip.js

---
+++ cmds/flip.js
@@ -0,0 +1,19 @@
+
+const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᄅƐㄣϛ9ㄥ86:;<=>?@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
+// Start with the character '!'
+const OFFSET = '!'.charCodeAt(0);
+
+exports.run = (bot, message, args) => {
+    if (args.length < 1) return message.channel.send("You must provide text to flip!");
+
+    message.channel.send(
+        args.join(' ').split('')
+            .map(c => c.charCodeAt(0) - OFFSET)
+            .map(c => mapping[c] || ' ')
+            .reverse().join('')
+    );
+}
+
+module.exports.help = {
+    name: "flip"
+}
