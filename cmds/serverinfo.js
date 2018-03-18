3b72ba3746cc83ff3d1820c8d8c26cefbf91cc99
Odar <radoradev770@gmail.com>
2018-03-18 23:08:46 +0200

    Update serverinfo.js

--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -1,19 +1,30 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  const verlvl = {
+    0: "None",
+    1: "Low",
+    2: "Medium",
+    3: "(╯°□°）╯︵ ┻━┻",
+    4: "(ノಠ益ಠ)ノ彡┻━┻"
+  }
+
     let inline = true
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
     .setColor("#00ff00")
     .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name, inline)
-    .addField("Server Onwer", message.guild.owner, inline)
-    .addField("Server Region", message.guild.region, inline)
-    .addField("Verification Level", message.guild.verificationLevel,inline)
-    .addField("Total Members", message.guild.memberCount, inline)
+    .setAuthor(message.guild.name)
+    .addField("Name", message.guild.name, inline)
+    .addField("ID", message.guild.id, inline)
+    .addField("Onwer", message.guild.owner, inline)
+    .addField("Region", message.guild.region, inline)
+    .addField("Verification Level",`${verlvl[message.guild.verificationLevel]}`,inline)
+    .addField("Members", `<:user:424958082691629057> ${message.guild.memberCount}`, inline)
     .addField("Roles", message.guild.roles.size, inline)
+    .addField("Channels", message.guild.channels.size, inline)
     .addField("You Joined", message.member.joinedAt)
-    .setFooter(`Created On ${message.guild.createdAt}`)
+    .setFooter(`Created ${message.guild.createdAt}`)
 
     message.channel.send(serverembed);
 
