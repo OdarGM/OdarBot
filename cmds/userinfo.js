f2fdd544226c4ca1b590ec53efa5467c1a39a7c1
Odar <radoradev770@gmail.com>
2018-03-20 13:54:07 +0200

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -20,18 +20,19 @@ if (member.user.bot === true) {
   }
 
             let embed = new Discord.RichEmbed()
-                .setAuthor(member.user.username)
+                //.setAuthor(member.user.username)
                 .setThumbnail((target.displayAvatarURL))
                 .setColor("#00ff00")
-                .addField("Full Username", 
-    `${member.user.tag}`, inline)
+                .addField("Full Username", `${member.user.tag}`, inline)
                 .addField("ID", member.user.id, inline)
                 .addField("Nickname", `${member.nickname !== null ? `<:approve:424890484025196565> Nickname: ${member.nickname}` : "<:deny:424890501758451712> None"}`, true)
                 .addField("Bot", `${bot}`,inline, true)
                 .addField("Status", `${status[member.user.presence.status]}`, inline, true)
-                .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "<:deny:424890501758451712> Not playing"}`,inline, true)
+                .addField("Playing", `${member.user.presence.game ? `🎮 ${member.user.presence.game.name}` : "<:deny:424890501758451712> Not playing"}`,inline, true)
                 .addField("Roles", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "<:deny:424890501758451712> No Roles"}`, true)
                 .addField("Joined Discord At", member.user.createdAt)
+                .setFooter(`Information about ${member.user.username}`)
+                .setTimestamp()
     
             message.channel.send(embed);
 
