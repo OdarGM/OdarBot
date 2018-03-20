1e83e726afe99c6fae9e9e9fc792796cde33e57c
Odar <radoradev770@gmail.com>
2018-03-20 13:47:52 +0200

    Add files via upload

---
+++ cmds/mock.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+
+exports.run = (bot, message, args) => {
+    if (args.length < 1) return message.channel.send("Please provide some text to Mock")
+
+    let mockEmbed = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .setDescription(args.map(randomizeCase))
+    .setImage("https://cdn.discordapp.com/attachments/424889733043191810/425242569325150208/mock.jpg")
+
+    message.channel.send(mockEmbed)
+
+    message.delete();
+
+}
+
+module.exports.help = {
+    name: "mock"
+}
