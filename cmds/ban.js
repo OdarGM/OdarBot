6a270480f4577843a7bbf6210972cfb61041f593
Odar <radoradev770@gmail.com>
2018-03-23 13:24:40 +0200

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,10 +1,16 @@
-﻿const Discord = require("discord.js")
+const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
 if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry, you don't have permissions to use this!");
-let logs = message.guild.channels.find('name', 'logs');
-if (!logs) return message.reply('I cannot find the logs channel');
+if(args[0] == "help"){
+  let banhelp = new Discord.RichEmbed()
+  .setColor("#00ff00")
+  .addField("Ban Command", "Usage: !ban <@user> <reason>")
+
+  message.channel.send(banhelp);
+  return;
+}
     
     let member = message.mentions.members.first();
     if(!member)
@@ -14,28 +20,16 @@ if (!logs) return message.reply('I cannot find the logs channel');
 
     let reason = args.slice(1).join(' ');
     if(!reason)
-      return message.reply("Please indicate a reason for the ban!");
+      return message.reply("Please  a reason for the ban!");
     
     await member.ban(reason)
       .catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.username} has been banned by ${message.author.username} because: **__${reason}__**`);
-
-    let Banembed = new Discord.RichEmbed()
-    .setColor("#00ff00")
-    .setTimestamp()
-    .addField('Action:', 'Ban')
-    .addField('User:', `${member.user.username}#${member.user.discriminator} (${member.user.id})`)
-    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
-    .addField('Reason', reason);
-
-    logs.send(Banembed);
+    message.reply(`${member.user.username} has been **__banned__** from the server!`);
 
     message.delete();
 
       }
       module.exports.help = {
-        name: "ban",
-        type: "admin",
-        description: "Bans a member",
-        usage: "!ban <@user> [reason]"
+        name: "ban"
       }
+
