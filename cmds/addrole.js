1abfbe31f94f4df3f327c3f921ddba9d67c25afd
Odar <radoradev770@gmail.com>
2018-03-29 22:45:16 +0300

    Update addrole.js

--- cmds/addrole.js
+++ cmds/addrole.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole <@user> <Role> 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have premmsions to do that!");
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You don't have premmsions to do that!");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Couldn't find that user!");
   let role = args.join(" ").slice(22);
