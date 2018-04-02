5f52a1275cda2884347cff342221ab2d775aaf0c
Odar <radoradev770@gmail.com>
2018-04-02 13:45:16 +0300

    Clearing Spaces

--- bot.js
+++ bot.js
@@ -49,10 +49,7 @@ try {
 
 bot.on("message", async message => {
     if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    
-
-    
+    if(message.channel.type === "dm") return;  
 
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
