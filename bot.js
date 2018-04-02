6630328bee8de64f93972ae8aaa863aa11bdd0d9
Odar <radoradev770@gmail.com>
2018-04-02 13:44:02 +0300

    another dbl test
    hmm

--- bot.js
+++ bot.js
@@ -51,9 +51,7 @@ bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
     
-    setInterval(() => {
-        dbl.postStats(client.guilds.size);
-    }, 1800000);
+
     
 
     let messageArray = message.content.split(" ");
