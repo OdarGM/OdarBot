5fea52a29d5a838d144faa18176fd9d0be8b194a
Odar <radoradev770@gmail.com>
2018-04-10 16:12:22 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -3,28 +3,55 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
 if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry, you don't have permissions to use this!");
-if(args[0] == "help"){
-  let banhelp = new Discord.RichEmbed()
-  .setColor("#00ff00")
-  .addField("Ban Command", "Usage: !ban <@user> <reason>")
+if(args[0] == "help"){ return
 
-  message.channel.send(banhelp);
-  return;
+  let xdemb = new Discord.RichEmbed()
+  .setColor("#00ff00")
+  .setTitle("Ban Command")
+  .addField("Description:", `Ban a member`, true)
+  .addField("Usage:", "!ban [user] [reason]", true)
+  .addField("Example:", "!ban @Odar spam")
+
+  message.channel.send(xdemb);
+  
 }
-    
+
+let xdemb = new Discord.RichEmbed()
+.setColor("#00ff00")
+.setTitle("Ban Command")
+.addField("Description:", `Ban a member`, true)
+.addField("Usage:", "!ban [user] [reason]", true)
+.addField("Example: !ban @Odar spam")
+
+
     let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention member to ban!");
+    if(!member) return message.channel.send(xdemb)
     if(!member.bannable) 
-      return message.reply("I cannot ban this user!");
+      return message.channel.send("I cannot ban this user!");
 
     let reason = args.slice(1).join(' ');
-    if(!reason)
-      return message.reply("Please  a reason for the ban!");
+
+    if(!reason) {
+      res = "No reason given";
+    }
+    else {
+      res = `${reason}`
+    }
     
     await member.ban(reason)
       .catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.username} has been **__banned__** from the server!`);
+
+
+      let bean = new Discord.RichEmbed()
+      .setColor("#00ff00")
+      .setTitle(`Ban | ${member.user.tag}`)
+      .addField("User", member, true)
+      .addField("Moderator", message.author, true)
+      .addField("Reason", res)
+      .setTimestamp()
+      .setFooter(member.id)
+
+      message.channel.send(bean)
 
     message.delete();
 
@@ -32,4 +59,3 @@ if(args[0] == "help"){
       module.exports.help = {
         name: "ban"
       }
-
