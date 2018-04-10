37e1fbaa7be229a359708a2ad6cc2190b8c56ba8
Odar <radoradev770@gmail.com>
2018-04-10 16:06:01 +0300

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  if(args[0] == "help"){
+    let helpembxd = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .addField("clear Command", "Usage: !Clear <amount>")
+
+    message.channel.send(helpembxd);
+    return;
+  } 
+
+  message.delete()
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premssions to do that!");
   if(!args[0]) return message.channel.send("Please enter a number of messages to clear! `Usage: !clear <amount>`");
