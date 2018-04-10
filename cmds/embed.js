634770a3a0d70d7dd0245363f3e1e5eee911a0fa
Odar <radoradev770@gmail.com>
2018-04-10 16:06:55 +0300

    Add files via upload

---
+++ cmds/embed.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+  if(message.author.id !== "291221132256870400") return;
+
+  const cmd = args.join(' ').split(' | ')
+
+  let emb = new Discord.RichEmbed()
+  .setTitle(cmd[0])
+  .setColor(cmd[1])
+  .setDescription(cmd[2])
+  .setTimestamp()
+
+  message.channel.send(emb)
+
+    }
+    module.exports.help = {
+        name: "embed"
+      }
