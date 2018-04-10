350042a93cde045297718c0e587fc38b6ee19c2d
Odar <radoradev770@gmail.com>
2018-04-10 16:12:01 +0300

    Update removerole.js

--- cmds/removerole.js
+++ cmds/removerole.js
@@ -1,18 +1,36 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You need the `manage members`premission to do that!.");
-  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user.");
-  let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
-  let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(args[0] == "help"){
+    let helpembxd = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .addField("Removerole Command", "Usage: !removerole <@user> <role>")
 
-  if(!rMember.roles.has(gRole.id)) return message.reply("This user doesn't have that role.");
+    message.channel.send(helpembxd);
+    return;
+  } 
+
+  let xdemb = new Discord.RichEmbed()
+  .setColor("#00ff00")
+  .setTitle(`Removerole command`)
+  .addField("Description:", "Take role from member", true)
+  .addField("Usage", "!removerole [user] [role]", true)
+  .addField("Example", "!removerole @Odar Member")
+
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You need the `manage members`premission to do that!.");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.channel.send(xdemb);
+
+  let role = args.join(" ").slice(22);
+
+  if(!role) return message.channel.send("Specify a role!");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.channel.send("Couldn't find that role.");
+
+  if(!rMember.roles.has(gRole.id)) return message.channel.send("This user doesn't have that role.");
   await(rMember.removeRole(gRole.id));
 
-  await message.channel.send(`***I just removed ${rMember}'s ${gRole.name} role!***`)
+  await message.channel.send(`***I just removed ${rMember.user.username}'s ${gRole.name} role!***`)
 
   message.delete();
 
