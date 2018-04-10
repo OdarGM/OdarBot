ac8e9183ffc67676fd9d99441b414fc360e1d204
Odar <radoradev770@gmail.com>
2018-04-10 16:08:11 +0300

    Update buy.js

--- cmds/buy.js
+++ cmds/buy.js
@@ -10,10 +10,10 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Buy Discord Bot!")
     .setThumbnail(bicon)
     .addField("You can order your own discord bot from", "<@291221132256870400>")
-    .addField("Bot Commands", "Same as this bot", inline)
+    .addField("Bot Commands", "Custom commands", inline)
     .addField("XP System", "Optional", inline)
     .addField("Private bot", "Yes only the bot owner can invite it!")
-    .addField("Pricing", "€20 Per Bot", inline)
+    .addField("Pricing", "€15 Per Bot", inline)
     .addField("Order", "To order the bot use `!order`", inline)
     .addField("Questions", "If you have any questions feel free to ask in the [Support Server](https://discord.gg/zvvasbc)")
 
