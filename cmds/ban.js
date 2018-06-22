073acd5fb2b5152df9f67a744bdf20674d80cf5c
Odar <radoradev770@gmail.com>
2018-06-22 20:31:43 +0300

    after beign banned over 200 times with my own bot i am fixing this

--- cmds/ban.js
+++ cmds/ban.js
@@ -2,61 +2,43 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry, you don't have permissions to use this!");
-if(args[0] == "help"){ return
-
-  let xdemb = new Discord.RichEmbed()
-  .setColor("#00ff00")
-  .setTitle("Ban Command")
-  .addField("Description:", `Ban a member`, true)
-  .addField("Usage:", "!ban [user] [reason]", true)
-  .addField("Example:", "!ban @Odar spam")
-
-  message.channel.send(xdemb);
-  
-}
-
 let xdemb = new Discord.RichEmbed()
-.setColor("#00ff00")
-.setTitle("Ban Command")
-.addField("Description:", `Ban a member`, true)
-.addField("Usage:", "!ban [user] [reason]", true)
-.addField("Example: !ban @Odar spam")
+        .setColor("#00ff00")
+        .setTitle("Ban Command")
+        .addField("Description:", `Ban a member`, true)
+        .addField("Usage:", `${prefix}ban [user] [reason]`, true)
+        .addField("Example:", `${prefix}ban @Odar spam`)
 
+        if(!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "291221132256870400") return message.channel.send("Sorry you don't have premssions to use this!");
 
-    let member = message.mentions.members.first();
-    if(!member) return message.channel.send(xdemb)
-    if(!member.bannable) 
-      return message.channel.send("I cannot ban this user!");
+        let member = message.mentions.members.first();
+        if(!member) return message.channel.send(xdemb)
+        if(!member.bannable) return message.channel.send("I can't ban this user!")
+        if(member.user.id === "291221132256870400") return message.channel.send("I can't ban my owner!")
 
-      if(member.id === message.author.id)
-      return message.channel.send("You can't kick your self!")
+        if(member.id === message.author.id) return message.channel.send("You can't ban your self")
 
-    let reason = args.slice(1).join(' ');
+        let reason = args.slice(1).join(" ");
 
-    if(!reason) {
-      res = "No reason given";
-    }
-    else {
-      res = `${reason}`
-    }
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
+        if(!reason) {
+            res = "No reason given";
+        } else {
+            res = reason
+        }
 
+        await member.ban(reason).catch(error => message.channel.send(`Sorry, I coldn't ban because of: ${error}`));
 
-      let bean = new Discord.RichEmbed()
-      .setColor("#00ff00")
-      .setTitle(`Ban | ${member.user.tag}`)
-      .addField("User", member, true)
-      .addField("Moderator", message.author, true)
-      .addField("Reason", res)
-      .setTimestamp()
-      .setFooter(member.id)
+        let bean = new Discord.RichEmbed()
+        .setColor("#00ff00")
+        .setTitle(`Ban | ${member.user.tag}`)
+        .addField("User", member, true)
+        .addField("Moderator", message.author, true)
+        .addField("Reason", res)
+        .setTimestamp()
 
-      message.channel.send(bean)
+        message.channel.send(bean)
 
-    message.delete();
+        message.delete()
 
       }
       module.exports.help = {
