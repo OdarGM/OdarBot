eae49933b69f2a1a22279eb8277176d9b4c87351
Odar <radoradev770@gmail.com>
2018-07-29 23:07:46 +0300

    Create alpho.js

---
+++ cmds/alpho.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+message.channel.send("Nub")
+
+}
+
+module.exports.help = {
+    name: "alpho"
+}
