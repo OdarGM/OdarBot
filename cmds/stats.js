211bbad20914ae441fc5e39df8655ec77ae75728
Odar <radoradev770@gmail.com>
2018-08-23 22:28:36 +0300

    Update stats.js

--- cmds/stats.js
+++ cmds/stats.js
@@ -12,13 +12,18 @@
               if (err) {
                 return console.log(err);
               }
+             
+             let secs = Math.floor(bot.uptime % 60);
+             let days = Math.floor((bot.uptime % 31536000) / 86400);
+             let hours = Math.floor((bot.uptime / 3600) % 24);
+             let mins = Math.floor((bot.uptime / 60) % 60);
      
-              let duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+              //let duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
               let embedStats = new Discord.RichEmbed()
              .setTitle("*** Stats ***")
              .setColor("#00ff00")
              .addField("• Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
-             .addField("• Uptime ", `${duration}`, true)
+             .addField("• Uptime ", `${hours}h ${mins}m`, true) //`${duration}`, true)
              .addField("• Users", `${bot.users.size.toLocaleString()}`, true)
              .addField("• Servers", `${bot.guilds.size.toLocaleString()}`, true)
              .addField("• Channels ", `${bot.channels.size.toLocaleString()}`, true)
