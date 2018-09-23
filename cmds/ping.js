675466381b0c2ed84a7804089822250cbe8243e7
Odar <radoradev770@gmail.com>
2018-09-23 21:29:42 +0300

    Update ping.js
    Rounded the  bot ping cmd

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-message.channel.send('Pong! `' + bot.ping + '`ms')
+message.channel.send('Pong! `' + Math.floor(Math.round(bot.ping)) + '`ms')
 
 message.delete();
 
