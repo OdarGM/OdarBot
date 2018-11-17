b3fcbfb4b3859fb294da3a7485da9a1a9401345c
Odar <radoradev770@gmail.com>
2018-11-17 20:23:34 +0200

    Update say.js

--- cmds/say.js
+++ cmds/say.js
@@ -1,5 +1,8 @@
 module.exports.run = async (bot, message, args) => {
     
+    if(message.author.id === "448410421339095041") return
+    if(message.author.id === "485494651835383828") return
+    
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
