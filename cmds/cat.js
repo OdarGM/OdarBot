f72b0aad238e17d6fbcb3dfa19b9355d56205578
Odar <radoradev770@gmail.com>
2018-11-23 23:02:06 +0200

    Update cat.js

--- cmds/cat.js
+++ cmds/cat.js
@@ -1,10 +1,14 @@
 const Commando = require('discord.js-commando');
 const request = require('request'); 
 
+const Discord = require("discord.js")
+
 module.exports.run = async (bot, message, args) => {
         request('http://edgecats.net/random', function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                return message.reply(body);
+                    let emb = new Discord.RichEmbed()
+                    .setImage(body)
+                    .setColor(""#00ff00")
             }
         });
     }
