bdc1410139da9621c5d9edf60ddab6c45d65b238
Adrian <56496903+Adrian-Cristian@users.noreply.github.com>
2020-09-01 12:01:36 -0300

    morse altered
    if the user does not type anything, he will warn something

--- cmds/morse.js
+++ cmds/morse.js
@@ -5,6 +5,8 @@ module.exports.run = async (bot, message, args) => {
     let alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split(""),
 				morse = "/,.-,-...,-.-.,-..,.,..-.,--.,....,..,.---,-.-,.-..,--,-.,---,.--.,--.-,.-.,...,-,..-,...-,.--,-..-,-.--,--..,.----,..---,...--,....-,.....,-....,--...,---..,----.,-----".split(","),
 				text = args.join(" ").toUpperCase();
+	               if (!text) return message.channel.send('Place a text or a morse code to be encoded or decoded.') // but you can change the answer :)
+	
 			while (text.includes("Ä") || text.includes("Ö") || text.includes("Ü")) {
 				text = text.replace("Ä","AE").replace("Ö","OE").replace("Ü","UE");
 			}
